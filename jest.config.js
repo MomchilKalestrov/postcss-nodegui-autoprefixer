5f29f68c0ccf269990e042e73d3e8a4b424db0b4
Momchil Kalestrov <kalestrov.mo@gmail.com>
2026-04-16 09:04:16 +0300

    Update the jest config using `ts-jest config:update jest.config.js`

--- jest.config.js
+++ jest.config.js
@@ -1,17 +1,30 @@
 module.exports = {
   clearMocks: true,
-  coverageDirectory: "coverage",
-  globals: {
-    "ts-jest": {
-      tsConfig: "tsconfig.json"
-    }
-  },
-  moduleFileExtensions: ["js", "ts", "tsx"],
-  testEnvironment: "node",
-  testMatch: ["**/__tests__/*.+(ts|tsx|js)"],
-  testPathIgnorePatterns: ["dist"],
+  coverageDirectory: 'coverage',
+  moduleFileExtensions: [
+    'js',
+    'ts',
+    'tsx',
+  ],
+  testEnvironment: 'node',
+  testMatch: [
+    '**/__tests__/*.+(ts|tsx|js)',
+  ],
+  testPathIgnorePatterns: [
+    'dist',
+  ],
   transform: {
-    "^.+\\.(ts|tsx)$": "ts-jest"
+    '^.+\\.(ts|tsx)$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
+    '^.+\\.tsx?$': [
+      'ts-jest',
+      {
+        tsconfig: 'tsconfig.json',
+      },
+    ],
   },
-  preset: "ts-jest"
-};
+}
